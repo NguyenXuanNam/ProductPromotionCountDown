9f32270bf1a0a711272d1444ebcd6e7784778ae2
NamNguyen <xuannam0205@gmail.com>
2021-05-11 01:00:33 +0700

    - init source

--- registration.php
+++ registration.php
@@ -1,1 +1,10 @@
 <?php
+/**
+ * @author Nguyen Xuan Nam
+ * Copyright © xuannam0205@gmail.com, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Nam_ProductPromotionCountDown', __DIR__);
